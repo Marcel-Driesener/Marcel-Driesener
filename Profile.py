88da07bd88a5b2a85b5549ee052396a629208d2d
Marcel Driesener <141312663+Marcel-Driesener@users.noreply.github.com>
2025-09-01 15:29:32 +0200

    starting the fine tuning

--- Profile.py
+++ Profile.py
@@ -25,31 +25,28 @@ codingyears, codingmonths, codingdays = calculate_delta(coding_start_date)
 
 # fill Markdown
 markdown = f"""
-<img align="right" height="300" src="assets/Rikka.gif" alt="Rikka Takanashi 'dance'" />
-
 ### Marcel Driesener
 
 ---
 
 <br>
 <br>
+<p align="right">gif from: Love, Chunibyo & Other Delusions</p>
+<img align="right" height="300" src="assets/Rikka.gif" alt="Rikka Takanashi 'dance'" />
 
-|                            |                                                                                                           |
-| -------------------------- | --------------------------------------------------------------------------------------------------------- |
+|                       |                                                                                                      |
+| --------------------- | ---------------------------------------------------------------------------------------------------- |
 | OS                    | Windows, Linux, Android                                                                              |
 | Runtime               | {pluralize(years, 'year')}, {pluralize(months, 'month')}, {pluralize(days, 'day')}                   |
 | Coding Since          | {pluralize(codingyears, 'year')}, {pluralize(codingmonths, 'month')}, {pluralize(codingdays, 'day')} |
 | Hobbies               | BMX, Building Keyboards, Gaming                                                                      |
 | Fun Fact              | I have 4 Self build Keyboards from SplitKB.com                                                       |
 | Fun Fact              | My Favorite from them is the Aurora Sweep                                                            |
-| ‎                          | ‎                                                                                                         |
+| ‎                     | ‎                                                                                                    |
 | Frameworks & tools    | React, Vite, Node.js                                                                                 |
 | languages.programming | JavaScript, TypeScript, Python, Terraform                                                            |
 | languages.real        | German, English                                                                                      |
 
-<br>
-<br>
-<p align="right">gif from: Love, Chunibyo & Other Delusions</p>
 """
 
 with open("README.md", "w", encoding="utf-8") as f:
